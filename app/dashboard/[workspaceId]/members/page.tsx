5ba2b35f830ea854667c4a20e013450f7f5d8c09
trace2798 <113078518+trace2798@users.noreply.github.com>
2025-07-10 14:27:26 +0530

    working on members table

--- app/dashboard/[workspaceId]/members/page.tsx
+++ app/dashboard/[workspaceId]/members/page.tsx
@@ -1,11 +1,34 @@
 import { FC } from "react";
+import { columns, Payment } from "./_components/columns";
+import { DataTable } from "./_components/data-table";
+import { Separator } from "@/components/ui/separator";
 
 interface PageProps {}
 
-const Page: FC<PageProps> = ({}) => {
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+  ];
+}
+
+const Page: FC<PageProps> = async ({}) => {
+  const data = await getData();
+
   return (
     <>
-      <div>Page</div>
+      <div className="flex flex-col space-y-10 w-full max-w-6xl mx-auto">
+        <div className="flex flex-col space-y-5">
+          <h1 className="text-5xl">Current Workspace Members</h1>
+          <Separator />
+        </div>
+        <DataTable columns={columns} data={data} />
+      </div>
     </>
   );
 };
